f38ece3e83fa35b04c5b1473632d523c168ff6f3
tomo_s <f15c017d@gmail.com>
2020-01-26 18:37:38 +0900

    羊を数えるアプリを作ってみた
    - 参考サイト
    
      - https://qiita.com/TomiGie/items/fb60a80780689b86dc59

--- app/src/main/java/tomo/myapplication/MainActivity.kt
+++ app/src/main/java/tomo/myapplication/MainActivity.kt
@@ -2,11 +2,43 @@ package tomo.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    var sheepCount = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val trialTime = Date()
+        var greeting = "こんばんは"
+        val calendar = GregorianCalendar()
+        calendar.time = trialTime
+
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+        if (hour in 3..11){
+            greeting = "おはよう"
+        } else if (hour > 15){
+            greeting = "こんばんは"
+        }
+
+        greeting += "ねむれませんか？"
+
+        val sheepNum = 10
+
+        for (i in 1..sheepNum){
+            greeting += "羊が" + i + "匹\n"
+        }
+
+        textview.text = greeting
+
+        rootLayout.setOnClickListener {
+            sheepCount++
+            val sheepText = "羊が $sheepCount 匹"
+            textview.text = sheepText
+        }
+
     }
 }
